b50bb49b495542c1957a097c2954a3d09c773e59
Iskhak <abdusalamoviskhak@gmail.com>
2024-07-26 00:36:11 +0500

    19

---
+++ index.js
@@ -0,0 +1,19 @@
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	private: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", "");
+const b = +prompt("На сколько оцените его??", "");
+const c = prompt("Один из последних просмотренных фильмов?", "");
+const d = +prompt("Один из последних просмотренных фильмов?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
