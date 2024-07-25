e450553cf1dda5cc85b0234c5a15eab5e6968e6a
Iskhak <abdusalamoviskhak@gmail.com>
2024-07-26 00:37:25 +0500

    24

--- index.js
+++ index.js
@@ -8,12 +8,27 @@ const personalMovieDB = {
 	private: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", "");
-const b = +prompt("На сколько оцените его??", "");
-const c = prompt("Один из последних просмотренных фильмов?", "");
-const d = +prompt("Один из последних просмотренных фильмов?", "");
+for (let i = 0; i < 2; i++) {
+	const a = prompt("Один из последних просмотренных фильмов?", "");
+	const b = +prompt("На сколько оцените его??", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+   if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+		personalMovieDB.movies[a] = b;
+		console.log("Done");
+	} else {
+		console.log("Error");
+		i--;
+	}
+}
+
+if (personalMovieDB.count < 10) {
+	console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+	console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+	console.log("Вы киноман");
+} else {
+	console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
